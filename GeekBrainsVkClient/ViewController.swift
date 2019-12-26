fd0ab91b4c5d5ee5714fb6c557be4a8a96458361
Jasik <rogozhkin87@gmail.com>
2019-12-26 22:50:14 +0900

    added ios project for geekbrains

---
+++ GeekBrainsVkClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2019/12/26.
+//  Copyright © 2019 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
