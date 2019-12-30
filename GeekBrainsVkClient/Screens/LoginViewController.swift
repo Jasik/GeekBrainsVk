e217221fbdaa79eb43a1bf0f152aa3c80213c5e1
Jasik <rogozhkin87@gmail.com>
2019-12-30 22:37:00 +0900

    deleted ViewController and created LoginViewController

--- GeekBrainsVkClient/Screens/LoginViewController.swift
+++ GeekBrainsVkClient/Screens/LoginViewController.swift
@@ -1,20 +1,16 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  GeekBrainsVkClient
 //
-//  Created by Vladimir Rogozhkin on 2019/12/26.
+//  Created by Vladimir Rogozhkin on 2019/12/30.
 //  Copyright © 2019 Vladimir Rogozhkin. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
-
 }
-
