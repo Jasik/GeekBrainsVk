54e4a86ae5e4ce3f366762222458841798443406
Jasik <rogozhkin87@gmail.com>
2020-01-04 20:50:40 +0900

    добавлены инициализаторы для кастомного TextField

--- GeekBrainsVkClient/CustomVIews/LoginScreenTextField.swift
+++ GeekBrainsVkClient/CustomVIews/LoginScreenTextField.swift
@@ -12,4 +12,16 @@ open class LoginScreenTextField: UITextField {
     
     private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+    
 }
