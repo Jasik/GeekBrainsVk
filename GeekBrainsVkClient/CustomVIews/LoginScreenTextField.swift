ec850f3de71dd523efd9534e7f05efdd5d70cda8
Jasik <rogozhkin87@gmail.com>
2020-01-04 20:45:08 +0900

    Создание сабкласса бля UITextField

---
+++ GeekBrainsVkClient/CustomVIews/LoginScreenTextField.swift
@@ -0,0 +1,12 @@
+//
+//  LoginScreenTextField.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/01/04.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+open class LoginScreenTextField: UITextField {
+}
