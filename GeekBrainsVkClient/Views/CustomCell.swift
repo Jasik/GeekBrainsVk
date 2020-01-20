1c5c5a9b8d5fc156838fca7835cf25e32b070030
Jasik <rogozhkin87@gmail.com>
2020-01-21 01:49:23 +0900

    Добавлена кастомная ячейка для таблицы

---
+++ GeekBrainsVkClient/Views/CustomCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCell.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/01/20.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
