96df8cb90f448b02d0975c6488335d9bb64c1be6
Jasik <rogozhkin87@gmail.com>
2020-01-24 19:35:17 +0900

    Добавил 2 файла для Лайк метра и для тени

---
+++ GeekBrainsVkClient/Controls/LikeMeter.swift
@@ -0,0 +1,21 @@
+//
+//  LikeMeter.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/01/24.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class LikeMeter: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GeekBrainsVkClient/CustomVIews/ShadowView.swift
@@ -0,0 +1,9 @@
+//
+//  ShadowView.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/01/23.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
