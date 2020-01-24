ac644669a153b7b0e78c9eac1ab21609a2d8da78
Jasik <rogozhkin87@gmail.com>
2020-01-24 19:39:44 +0900

    Добавлена возможность настраивать из инспектора для тени аватарки

--- GeekBrainsVkClient/CustomVIews/ShadowView.swift
+++ GeekBrainsVkClient/CustomVIews/ShadowView.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class ShadowView: UIView {
+    
+    @IBInspectable
+    var shadowViewColor: UIColor = UIColor.white {
+        didSet {
+            updateShadowColor()
+        }
+    }
+    
+    @IBInspectable
+    var shadowViewRadius: CGFloat = 0 {
+        didSet {
+            updateShadowRadius()
+        }
+    }
+    
+    @IBInspectable
+    var shadowViewOpacity: Float = 0 {
+        didSet {
+            updateShadowOpacity()
+        }
+    }
+    
+    private func updateShadowColor() {
+        self.layer.shadowColor = shadowViewColor.cgColor
+    }
+    
+    private func updateShadowRadius() {
+        self.layer.shadowRadius = shadowViewRadius
+    }
+    
+    private func updateShadowOpacity() {
+        self.layer.shadowOpacity = shadowViewOpacity
+    }
+}
