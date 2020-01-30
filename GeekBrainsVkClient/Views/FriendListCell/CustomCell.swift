e372b7dbc22cf8ff4eb5e985f7ff63a1ae806036
Jasik <rogozhkin87@gmail.com>
2020-01-30 22:26:56 +0900

    Updated Custom cell

---
+++ GeekBrainsVkClient/Views/FriendListCell/CustomCell.swift
@@ -0,0 +1,45 @@
+//
+//  CustomCell.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/01/20.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet private weak var thumbnailImageView: UIImageView!
+    @IBOutlet private weak var TitleLabel: UILabel!
+    @IBOutlet private weak var shadowView: ShadowView!
+    
+    var friend: User? {
+        didSet {
+            guard let friend = friend else {
+                return
+            }
+            thumbnailImageView.image = UIImage(named: friend.image.first ?? "00")
+            TitleLabel.text = friend.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupImageUI()
+        setupShadowView()
+    }
+    
+    func setupImageUI() {
+        thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+    }
+    
+    func setupShadowView() {
+        shadowView.layer.cornerRadius = shadowView.bounds.width / 2
+        shadowView.backgroundColor = .white
+        shadowView.layer.borderWidth = 1
+        shadowView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        shadowView.layer.shadowOffset = .zero
+    }
+}
