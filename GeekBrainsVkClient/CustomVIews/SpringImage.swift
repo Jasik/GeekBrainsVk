075b39c5c65172353e8f61299fe9c3fb4bc8be4f
Jasik <rogozhkin87@gmail.com>
2020-02-05 21:56:59 +0900

    Создал файлы для анимации загрузки и пружжинной анимации фото

---
+++ GeekBrainsVkClient/CustomVIews/SpringImage.swift
@@ -0,0 +1,21 @@
+//
+//  SpringImage.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/02/05.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class SpringImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GeekBrainsVkClient/Screens/SplashView.swift
@@ -0,0 +1,30 @@
+//
+//  SplashView.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/02/04.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class SplashView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
