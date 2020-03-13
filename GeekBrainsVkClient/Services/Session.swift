626f1c3e699aa27944c3a1cd2888f90e5bb6269d
Jasik <rogozhkin87@gmail.com>
2020-03-13 20:50:59 +0900

    Created Session

---
+++ GeekBrainsVkClient/Services/Session.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/13.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
