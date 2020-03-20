3c4f0712c374f86866a8222f768c607e2131d567
Jasik <rogozhkin87@gmail.com>
2020-03-20 17:09:27 +0900

    Created Group model

---
+++ GeekBrainsVkClient/Model/Group.swift
@@ -0,0 +1,9 @@
+//
+//  Group.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/20.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
