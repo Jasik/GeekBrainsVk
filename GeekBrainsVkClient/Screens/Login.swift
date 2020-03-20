5150484790ae04270a863b232bd0bc8d7a44a67a
Jasik <rogozhkin87@gmail.com>
2020-03-20 15:05:22 +0900

    created Login and API files

---
+++ GeekBrainsVkClient/Screens/Login.swift
@@ -0,0 +1,30 @@
+//
+//  Login.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/16.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GeekBrainsVkClient/Services/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/17.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
