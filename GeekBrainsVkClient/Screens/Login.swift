f0ad4bd9ad627ad95f81b749536dbe4ce92f8ebc
Jasik <rogozhkin87@gmail.com>
2020-03-20 15:15:52 +0900

    added delegate for webkit

--- GeekBrainsVkClient/Screens/Login.swift
+++ GeekBrainsVkClient/Screens/Login.swift
@@ -37,3 +37,44 @@ class Login: UIViewController {
     }
     
 }
+
+extension Login: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationResponse: WKNavigationResponse,
+                 decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard
+            let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment
+        else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+            }
+        
+        let token = params["access_token"]
+        
+        /// TODO: delete
+        print(token)
+        
+        if let token = token {
+            session.token = token
+        }
+        
+        transitionAfterReceivingAToken()
+
+        decisionHandler(.cancel)
+    }
+}
