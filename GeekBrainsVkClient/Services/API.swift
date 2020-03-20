24117705f209b4a0d3c25c30d39e143c0b423c3b
Jasik <rogozhkin87@gmail.com>
2020-03-20 15:20:34 +0900

    added URLRequest for auth

--- GeekBrainsVkClient/Services/API.swift
+++ GeekBrainsVkClient/Services/API.swift
@@ -18,5 +18,23 @@ struct API {
     
     private let version = "5.103"
 
+    /// TODO: change to Alamofire later
+    mutating func logIn() -> URLRequest {
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "7360980"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "friends, photos, groups"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.103")
+        ]
+        
+        return URLRequest(url: urlComponents.url!)
+    }
     
 }
