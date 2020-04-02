5585228e0eb7275cc0c25c0db29377af880a84bc
Jasik <rogozhkin87@gmail.com>
2020-04-02 21:53:44 +0900

    Created Photo model

---
+++ GeekBrainsVkClient/Model/Photo.swift
@@ -0,0 +1,10 @@
+//
+//  Photo.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/28.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
+
