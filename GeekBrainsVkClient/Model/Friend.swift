b83d8e9ff76ced3c07024b5be3c5d090d9378f93
Jasik <rogozhkin87@gmail.com>
2020-04-02 21:50:36 +0900

    Created Friend model

---
+++ GeekBrainsVkClient/Model/Friend.swift
@@ -0,0 +1,10 @@
+//
+//  Friend.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/29.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
+
