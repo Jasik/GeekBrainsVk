c081962620d5ab3d4e3540325689bfbee0266e15
Jasik <rogozhkin87@gmail.com>
2020-04-02 21:55:30 +0900

    added and updated models for build

---
+++ GeekBrainsVkClient/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  GeekBrainsVkClient
+//
+//  Created by Vladimir Rogozhkin on 2020/03/21.
+//  Copyright © 2020 Vladimir Rogozhkin. All rights reserved.
+//
+
+import Foundation
+
+struct UserTest: Codable {
+    let id: Int
+    let name: String
+    let status: String
+}
