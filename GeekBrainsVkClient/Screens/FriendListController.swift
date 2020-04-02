2e0baf750ad8f89d41295c688b17629d84188c2c
Jasik <rogozhkin87@gmail.com>
2020-04-02 21:56:40 +0900

    Update FriendListController

--- GeekBrainsVkClient/Screens/FriendListController.swift
+++ GeekBrainsVkClient/Screens/FriendListController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class FriendListController: UIViewController {
 
@@ -15,13 +16,16 @@ class FriendListController: UIViewController {
     
     private let api = API()
     
-    private let friends: [User] = [
+    private let friendstest: [User] = [
            User(name: "Rogozhkin Vladimir", image: ["00", "00", "00"]),
            User(name: "Jonny Depp", image: ["j01", "j02", "j03"]),
            User(name: "Bred Pitt", image: ["b01", "b02", "b03"]),
            User(name: "Armano", image: ["00", "00", "00"]),
            User(name: "Jonny Pit", image: ["b01", "j02", "j03"])
     ]
+    
+    var friends: [Friend] = []
+    
     private var filteredData: [String: [User]] = [:]
     private var friendsDictionary = [String: [User]]()
     private var friendsSectionName = [String]()
@@ -38,7 +42,10 @@ class FriendListController: UIViewController {
         sortSectionTitle()
         setupSearchBar()
         
-        api.fetchFriendsList()
+        api.fetchFriendsList({ [weak self] friends in
+            self?.friends = friends
+            self?.tableView.reloadData()
+        })
         
         tableView.reloadData()
     }
@@ -63,7 +70,7 @@ class FriendListController: UIViewController {
             let key = friendsSectionName[selectedSection]
             if let friends = friendsDictionary[key] {
                 let friendsPhotoController = segue.destination as! FriendsPhotoController
-                friendsPhotoController.friend = friends[selectedCell]
+                friendsPhotoController.friendtest = friendstest[selectedCell]
             }
         }
     }
@@ -73,29 +80,43 @@ extension FriendListController: UITableViewDelegate { }
 
 extension FriendListController: UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return friendsSectionName.count
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return friendsSectionName.count
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let key = friendsSectionName[section]
-        if let friends = filteredData[key] {
-            return friends.count
-        }
-        return 0
+//        let key = friendsSectionName[section]
+//        if let friends = filteredData[key] {
+//            return friends.count
+//        }
+//        return 0
+        
+        return friends.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.className, for: indexPath) as? CustomCell else {
             fatalError()
         }
-        let key = friendsSectionName[indexPath.section]
-        if let friends = filteredData[key] {
-            let friend = friends[indexPath.row]
-            cell.thumbImageView.image = UIImage(named: friend.image.first ?? "00")
-            cell.TitleLabel.text = friend.name
-            cell.accessoryType = .disclosureIndicator
-        }
+//        let key = friendsSectionName[indexPath.section]
+//        if let friends = filteredData[key] {
+//            print("friend test  \(friends)")
+//            let friend = friends[indexPath.row]
+//            let url = URL(string: friend.photo100)
+//            let name = friend.lastName + friend.firstName
+////            cell.thumbImageView.image = UIImage(named: friend.image.first ?? "00")
+//            cell.thumbImageView.kf.setImage(with: url)
+//            cell.TitleLabel.text = name
+//            cell.accessoryType = .disclosureIndicator
+//        }
+        
+        let friend = friends[indexPath.row]
+        let url = URL(string: friend.photo100)
+        let name = friend.lastName + " " + friend.firstName
+
+        cell.TitleLabel.text = name
+        cell.thumbImageView.kf.setImage(with: url)
+        
         return cell
     }
     
@@ -103,13 +124,13 @@ extension FriendListController: UITableViewDataSource {
         return 80
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return friendsSectionName[section]
-    }
-    
-    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return friendsSectionName
-    }
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return friendsSectionName[section]
+//    }
+//
+//    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+//        return friendsSectionName
+//    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedSection = indexPath.section
@@ -122,7 +143,8 @@ extension FriendListController {
     
     private func getSectionTitle() {
         
-        for friend in friends {
+        for friend in friendstest {
+//          let friendKey = String(friend.lastName.prefix(1))
             let friendKey = String(friend.name.prefix(1))
             if var friendValues = friendsDictionary[friendKey] {
                 friendValues.append(friend)
@@ -155,6 +177,9 @@ extension FriendListController: UISearchBarDelegate {
         filteredData.removeAll()
         
         filteredData = searchText.isEmpty ? friendsDictionary : friendsDictionary.compactMapValues { user in
+//            user.filter({(friend: Friend) -> Bool in
+//            let name = friend.lastName + friend.firstName
+//            return name.range(of: searchText, options: .caseInsensitive) != nil
             user.filter({(friend: User) -> Bool in
                 return friend.name.range(of: searchText, options: .caseInsensitive) != nil
             })
