0de61bbe7a356d8d575e50acc0086fc3a446a2e8
Jasik <rogozhkin87@gmail.com>
2020-04-03 21:11:19 +0900

    Updated Group model to realm object

--- GeekBrainsVkClient/Model/Group.swift
+++ GeekBrainsVkClient/Model/Group.swift
@@ -7,26 +7,51 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct GroupResponse: Codable {
+class GroupResponse: Codable {
     let response: Groups
 }
 
-struct Groups: Codable {
+class Groups: Codable {
     let count: Int
     let items: [Group]
 }
 
-struct Group: Codable {
-    let id: Int
-    let isAdmin: Int
-    let isAdvertiser: Int
-    let isClosed: Int
-    let isMember: Int
-    let name: String
-    let photo100: String
-    let photo200: String
-    let photo50: String
-    let screenName: String
-    let type: String
+class Group: Object, Codable {
+    @objc dynamic let id: Int
+    @objc dynamic let isAdmin: Int
+    @objc dynamic let isAdvertiser: Int
+    @objc dynamic let isClosed: Int
+    @objc dynamic let isMember: Int
+    @objc dynamic let name: String
+    @objc dynamic let photo100: String
+    @objc dynamic let photo200: String
+    @objc dynamic let photo50: String
+    @objc dynamic let screenName: String
+    @objc dynamic let type: String
 }
+
+/// TODO: delete
+//struct GroupResponse: Codable {
+//    let response: Groups
+//}
+//
+//struct Groups: Codable {
+//    let count: Int
+//    let items: [Group]
+//}
+//
+//struct Group: Codable {
+//    let id: Int
+//    let isAdmin: Int
+//    let isAdvertiser: Int
+//    let isClosed: Int
+//    let isMember: Int
+//    let name: String
+//    let photo100: String
+//    let photo200: String
+//    let photo50: String
+//    let screenName: String
+//    let type: String
+//}
